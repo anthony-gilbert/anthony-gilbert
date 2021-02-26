6abb0377c31ee8363460e0b71285a95af6286707
Anthony <anthony.gilbert93@gmail.com>
2021-02-25 18:17:13 -0600

    Removed comments

--- components/Contact.vue
+++ components/Contact.vue
@@ -33,11 +33,8 @@
 
       </div>
 
-      <!-- Adding Text box -->
       <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="message" name="message" rows="3" required=""
       placeholder="message"></textarea>
-
-      <!-- Ending added Text box -->
 
 
       <div class="flex items-center justify-between">
