f44971e91e31c2949844281f71705db42152dab3
Anthony <anthony.gilbert93@gmail.com>
2021-02-25 19:25:27 -0600

    Added sticky footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
-  <div id="footer" class="container">
+  <footer id="footer" class="container w-full text-center border-t border-grey p-4 pin-b">
+      <div class="container">
     <p class="item1">Made with</p>&nbsp;
     <img class="item2" alt="Vue logo" src="../assets/logo.png" />
     <p class="item3">ue.js by Anthony Gilbert</p>
-  </div>
+      </div>
+  </footer>
 </template>
 
 
@@ -15,11 +17,25 @@ export default {}
 #footer {
   background-color: rgb(162, 160, 160);
   min-width: 100%;
+
+
+
+
 }
 img {
   width: 20px;
 }
 
+
+
+.footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    background-color: #f5f5f5;
+}
 
 .item1 {
   grid-column-start: 1;
---
+++ components/Spacer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="spacer">
+    </div>
+</template>
+
+<style>
+.spacer {
+    height: 100%;
+    padding-bottom: 7%;
+}
+</style>
