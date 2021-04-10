b7944cdeae5caaca21ce40a0b34d29a6c1b92231
Anthony <anthony.gilbert93@gmail.com>
2021-04-10 08:31:21 -0500

    Changed the order of the projects

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,6 +1,78 @@
 <template>
 <div>
 <div class="min-h-screen py-6 flex flex-col justify-center sm:py-12">
+
+
+<!--
+Personal Website - Nuxt.js
+-->
+
+  <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+    <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+    <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+      <div class="max-w-md mx-auto">
+        <div>
+          <img src="https://docs.dialoguewise.com/images/site/vuejs.png?width=9pc" class=" sm:w-27 md:w-32 lg:w-25" />
+          <h2 class="text-sm font-bold text-gray-900">My Personal Website</h2>
+        </div>
+        <div class="divide-y divide-gray-200">
+          <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+            <p>This is the site that you are currently on.</p>
+            <ul class="list-disc space-y-2">
+              <li class="flex items-start">
+                <span class="h-6 flex items-center sm:h-7">
+                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                  </svg>
+                </span>
+                <p class="ml-2">
+                  <code class="text-sm text-gray-900">Nuxt.js / Vue.js</code>
+                </p>
+              </li>
+              <li class="flex items-start">
+                <span class="h-6 flex items-center sm:h-7">
+                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                  </svg>
+                </span>
+                <p class="ml-2">
+                  <code class="text-sm text-gray-900">TailwindCSS</code>
+                </p>
+              </li>
+              <li class="flex items-start">
+                <span class="h-6 flex items-center sm:h-7">
+                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                  </svg>
+                </span>
+                <p class="ml-2">
+                  <code class="text-sm text-gray-900">TypeScript</code>
+                </p>
+              </li>
+            </ul>
+
+            <!-- <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p> -->
+          </div>
+          <div class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
+            <p>
+              <a target="_blank" href="https://anthony-gilbert.dev" class="visit text-cyan-600 hover:text-cyan-700">Visit App &rarr; </a>
+            </p>
+            <p>
+              <a target="_blank" href="https://github.com/anthonygilbertt/anthony-gilbert" class="visit text-cyan-600 hover:text-cyan-700">View Code &rarr; </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+<!--
+  Star Wars Responsive Fan Page
+-->
+
+
   <div class="relative py-3 sm:max-w-xl sm:mx-auto">
     <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
     <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
@@ -207,69 +279,6 @@ Github User Search App - Vue.js
 
 
 
-
-<!--
-Personal Website - Nuxt.js
--->
-
-  <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-    <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-    <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-      <div class="max-w-md mx-auto">
-        <div>
-          <img src="https://docs.dialoguewise.com/images/site/vuejs.png?width=9pc" class=" sm:w-27 md:w-32 lg:w-25" />
-          <h2 class="text-sm font-bold text-gray-900">My Personal Website</h2>
-        </div>
-        <div class="divide-y divide-gray-200">
-          <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-            <p>This is the site that you are currently on.</p>
-            <ul class="list-disc space-y-2">
-              <li class="flex items-start">
-                <span class="h-6 flex items-center sm:h-7">
-                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                  </svg>
-                </span>
-                <p class="ml-2">
-                  <code class="text-sm text-gray-900">Nuxt.js / Vue.js</code>
-                </p>
-              </li>
-              <li class="flex items-start">
-                <span class="h-6 flex items-center sm:h-7">
-                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                  </svg>
-                </span>
-                <p class="ml-2">
-                  <code class="text-sm text-gray-900">TailwindCSS</code>
-                </p>
-              </li>
-              <li class="flex items-start">
-                <span class="h-6 flex items-center sm:h-7">
-                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                  </svg>
-                </span>
-                <p class="ml-2">
-                  <code class="text-sm text-gray-900">TypeScript</code>
-                </p>
-              </li>
-            </ul>
-
-            <!-- <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p> -->
-          </div>
-          <div class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
-            <p>
-              <a target="_blank" href="https://anthony-gilbert.dev" class="visit text-cyan-600 hover:text-cyan-700">Visit App &rarr; </a>
-            </p>
-            <p>
-              <a target="_blank" href="https://github.com/anthonygilbertt/anthony-gilbert" class="visit text-cyan-600 hover:text-cyan-700">View Code &rarr; </a>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 
 
 
